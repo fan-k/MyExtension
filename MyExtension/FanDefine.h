4ba0a09eade7a36523ab297ddb89be64c93f6b47
樊康鹏 <15936219505@163.com>
2016-08-25 13:22:27 +0800

    tag--0.0.2

--- MyExtension/FanDefine.h
+++ MyExtension/FanDefine.h
@@ -46,10 +46,10 @@
 green:((float)((rgbValue & 0xFF00) >> 8)) / 255.0 \
 blue:((float)(rgbValue & 0xFF)) / 255.0 alpha:1.0]
 ///< format：22,22,22
-#define FRGBColor(r, g, b) ([UIColor colorWithRed:(r) / 255.0  \
+#define FRGBColor(r, g, b,alpha) ([UIColor colorWithRed:(r) / 255.0  \
 green:(g) / 255.0  \
 blue:(b) / 255.0  \
-alpha:1])
+alpha:alpha])
 ///< format：22,22,22,0.5
 #define kRGBAColor(r, g, b, a) ([UIColor colorWithRed:(r) / 255.0  \
 green:(g) / 255.0  \
